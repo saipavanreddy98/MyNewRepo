ef3b37ac1940c90fce2cc22c1a084bdb4ae0242d
saipavanreddy98 <102138969+saipavanreddy98@users.noreply.github.com>
2025-11-19 15:20:03 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,17 @@ public class HelloWorld {
     }
 }
 
+dev-2 push from local
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+
